568b6aa6ba9a41224b9340f796bf952a9464e302
b1nnnnid <suebenny2@gmail.com>
2025-03-28 20:35:01 +0900

    [0328] 유수빈 워크북 namespace

--- wordcount/wordcount_prj/wordcount/views.py
+++ wordcount/wordcount_prj/wordcount/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'wordcount/index.html')
 
 def word_count(request):
-    return render(request, 'word_count.html')
+    return render(request, 'wordcount/word_count.html')
 
 def result(request):
     entered_text = request.GET["fulltext"]  # 입력된 전체 텍스트
@@ -35,7 +35,7 @@ def result(request):
 
     return render(
         request,
-        "result.html",
+        "wordcount/result.html",
         {
             'alltext': entered_text,
             'dictionary': word_dictionary.items(),
@@ -50,4 +50,4 @@ def result(request):
 
 def hello(request):
     name_text = request.GET["nametext"]
-    return render(request,"hello.html",{'yourname':name_text})
+    return render(request,"wordcount/hello.html",{'yourname':name_text})
